a36e1dc5a1a8ca39d32aef58d89ac9cf3f376530
Medasxo <70206510+Medasxo@users.noreply.github.com>
2022-10-09 15:35:18 +0300

    Add a custom fetch data hook, modify filter component

--- savings_app_frontend/src/Filter.js
+++ savings_app_frontend/src/Filter.js
@@ -1,80 +1,30 @@
 import { Link, useNavigate } from 'react-router-dom';
 import "./Filter.css";
-import React from 'react';
+import "./useFetchData.js";
+import React, { useEffect } from 'react';
+import useFetchData from './useFetchData.js';
 
 
 
 const Filter = (props) => {
 
-    /*const handleChange = (e) => {
-        e.preventDefault();
-        props.setSearched(e.target.value);
-    };
-
-    const onSubmitTask = (e) => {
-        e.preventDefault();
-        props.setSearched(e.target.value);
-    }
-    */
-
-    const categories = {
-        freshFoods : [
-            "meat",
-            "vegetables",
-            "fruits",
-            "dairy",
-            "eggs and grains"
-        ],
-        madeFoods : [
-            "salads",
-            "soup",
-            "baked goods",
-            "pasta",
-            "meats"
-        ]
-    };
-
-    React.useEffect(() => {
-
-        
-        
-    }, []);
-
-    const [filters, setFilters] = React.useState({});
-
-    const freshFilter = () => {
-        setFilters(filters + {});
-    };
-
-    const madeFilter = () => {
-        setFilters(filters + ["made foods"]);
-    };
-
-    /*
-     *
-     * 
-      <div>
-                <input type="checkbox" onClick={freshFilter} />
-                <label>Fresh foods</label>
-                <input type="checkbox" onClick={madeFilter} />
-                <label>Made foods</label>
-            </div>
-
-            <div className={`${filters ? " right-panel-active" : ""}`}>
-
-            </div>
-     */
+    const [categories] = useFetchData("https://localhost:7183/api/categories");
 
     return (
 
         <>
-           
+            <div class="filterContainer">
+                {categories &&
+                    categories.map((category) => {
+                        return <p key={category.id} class="categories">{category.categoryName}</p>
+                    })}
+            </div>
         </>
-        
-    );
-    
 
-    
+    );
+
+
+
 
 
 };
