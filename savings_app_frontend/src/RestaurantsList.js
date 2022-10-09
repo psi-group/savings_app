4ca420599e6bc72952751f8f026a174c315ef263
Ne-Ga-TIV <anton.antonio.10@mail.ru>
2022-10-09 18:20:31 +0300

    Add images to the restaurants

--- savings_app_frontend/src/RestaurantsList.js
+++ savings_app_frontend/src/RestaurantsList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import "./List.css";
 
 const RestaurantsList = (props) => {
@@ -11,6 +11,7 @@ const RestaurantsList = (props) => {
                     <div className="container">
                         <a href={"/restaurant/" + restaurant.id}><span></span></a>
                         <li key={restaurant.id}>{restaurant.name}</li>
+                        <img className="container" src={restaurant.image} alt={restaurant.name} />
                     </div>))}
              </div>  
         </ul>
