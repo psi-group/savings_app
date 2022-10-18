6d2e8ab038a8c08df489b780663f4bc3b2265044
JustasMileika <justasmileika@gmail.com>
2022-10-19 01:58:16 +0300

    fixed bug

--- savings_app_frontend/src/Register.js
+++ savings_app_frontend/src/Register.js
@@ -36,7 +36,7 @@ const Register = () => {
                     
                 }
                 else {
-
+                    setErrMsg("");
                 }
             }).catch(error => {
 
