6bbea6165b4bfbb1006a45886643002c62eab26a
Medasxo <70206510+Medasxo@users.noreply.github.com>
2022-11-15 19:30:56 +0200

    add .vscode to gitignore

--- savings_app_frontend/src/CategoriesFilter/index.js
+++
@@ -1,4 +0,0 @@
-export { default } from "./CategoriesFilter";
-export { default as DropdownItem } from "./DropdownItem";
-export { default as DropdownMenu } from "./DropdownMenu";
-export { default as FilterDisplay } from "./FilterDisplay";
--- savings_app_frontend/src/Profile.js
+++
@@ -1,5 +0,0 @@
-const Profile = () => {
-    return <h1>Hello man</h1>
-}
-
-export default Profile;
