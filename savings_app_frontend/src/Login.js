83b3ed7b130645cc0e799b44667cb59116924b36
Medasxo <70206510+Medasxo@users.noreply.github.com>
2022-11-22 22:34:50 +0200

    change login page

--- savings_app_frontend/src/Login.js
+++ savings_app_frontend/src/Login.js
@@ -47,14 +47,14 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <div className="text-[20px] mx-72 flex justify-center mt-16 h-full flex-col">
-        <h1 className="text-center font-bold font-serif mb-3 text-2xl">Login</h1>
+    <div className="grid h-screen place-items-center pb-40 ">
+      <div className="text-[18px] w-[350px] sm:w-[400px] flex flex-col border-sky-500 border-2 px-6 py-3 rounded-xl shadow-xl">
+        <h1 className="text-center font-bold  mb-3 text-3xl">Log In</h1>
         <form onSubmit={handleRegister}>
           <div className="flex-col flex">
-            <label>Email:</label>
+            <label className="pl-1">Email:</label>
             <input
-              className=" border-2 border-black"
+              className=" border-2 border-sky-500 rounded-xl p-1"
               type="text"
               placeholder="Email"
               id="email"
@@ -63,9 +63,9 @@ const Login = () => {
             <br></br>
           </div>
           <div className="flex flex-col">
-            <label>Password:</label>
+            <label className="pl-1">Password:</label>
             <input
-              className=" border-2 border-black"
+              className=" border-2 border-sky-500 rounded-xl p-1"
               type="password"
               placeholder="Password"
               id="password"
@@ -73,13 +73,13 @@ const Login = () => {
             ></input>
             <br></br>
           </div>
-          <button className="mt-3 border-3 border-sky-800 font-bold hover:bg-sky-800 hover:text-white h-10 w-full">
+          <button className="mt-3 bg-sky-500 text-white rounded-xl font-bold hover:bg-sky-800 hover:text-white h-10 w-full">
             Sign In
           </button>
         </form>
-        <div className="mt-5 mb-5 bg-sky-800 h-[2px] w-full"></div>
+        <div className="mt-5 mb-5 bg-sky-500 h-[2px] w-full"></div>
       <h1 className="text-center font-bold font-serif">Don't have an account yet?</h1>
-      <Link to="/register" className="text-center text-sky-700">Register here</Link>
+      <Link to="/register" className="text-center text-sky-500 hover-sky:700">Register here</Link>
       </div>
       
     </div>
