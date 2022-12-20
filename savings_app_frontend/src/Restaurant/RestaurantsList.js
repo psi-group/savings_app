b8269938ce8bcb63696931a88e9cd2dd93aa909d
Medasxo <70206510+Medasxo@users.noreply.github.com>
2022-12-20 22:32:33 +0200

    fix restaurant list page

--- savings_app_frontend/src/Restaurant/RestaurantsList.js
+++ savings_app_frontend/src/Restaurant/RestaurantsList.js
@@ -12,13 +12,13 @@ const RestaurantsList = (props) => {
             to={"/restaurants/" + restaurant.id}
             className=" border-solid border-2 border-sky-500 
                                 float-left text-[20px] text-center text-black
-                                mb-[30px] p-[5px] ml-[3.5%] w-[260px] flex items-center
+                                mb-[30px] p-[5px] ml-[3.5%] w-[260px] flex items-center justify-center
                                 hover:shadow-[0_0_2px_1px_rgba(0,151,255,0.7)] font-bold rounded-lg h-[300px] "
           >
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-3 items-center justify-center">
             <li key={restaurant.id}>{restaurant.name}</li>
             <img
-              className=""
+              className="w-48 h-48"
               src={
                   restaurant.imageUrl == null ?
                       "https://savingsapp.blob.core.windows.net/userimages/profilePic.jpg" :
